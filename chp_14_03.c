8a2e66a9ac0ac3b12df4118455bfa0e4b0631a8f
vyas-bhakti27 <98168229+vyas-bhakti27@users.noreply.github.com>
2022-05-13 21:29:20 +0530

    Create chp_14_03.c

---
+++ chp_14_03.c
@@ -0,0 +1,17 @@
+// Q3)  Program to understand automatic variables
+
+
+
+#include<stdio.h>
+main ( )
+{
+int x=5;
+printf ("x=%d\t",x); // print value of x=5
+func ( ) ;   
+}
+func ( )   // calling a function 
+{
+int x=15;
+printf ("x=%d\t" ,x);  // print value of x=15
+}
+     
